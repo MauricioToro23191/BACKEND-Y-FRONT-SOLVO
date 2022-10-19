3b79a0c768e82dbdb85a805db140426a525ff854
josefernandez71 <josefernandez.8176@gmail.com>
2022-10-19 11:57:01 -0500

    new Update

--- Frontend/src/Component/forgotP.js
+++ Frontend/src/Component/forgotP.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function ForgotP({ title, TextCont, func,back }) {
+export default function ForgotP({ title, TextCont, func,back,placeH }) {
   return (
     <div id="formulario2">
       <form className="form2">
         <label className="backLogin" onClick={back}/>
         <label className="title">{title}</label>
         <label className="textTitle">{TextCont}</label>
-        <input className="Email" placeholder="Email"></input>
+        <input className="Email" placeholder={placeH}></input>
         <button className="nextStep" onClick={func} />
       </form>
     </div>
--- Frontend/src/Pages/login.js
+++ Frontend/src/Pages/login.js
@@ -6,121 +6,163 @@ import { useUsuario } from "../Context/ContextUser";
 import ForgotP from "../Component/forgotP";
 import ShowLogin from "../Component/Login";
 
-const API=process.env.REACT_APP_BACKEND
+const API = process.env.REACT_APP_BACKEND;
 const Login = () => {
-    const socket=useContext(SocketContext);
-    const [user, setuser] = useState("");
-    const [pass, setpass] = useState("");
-    const [style, setStyle] = useState("sideL");
-    const cambiarestado=async(id,user)=>{
-    const res =await fetch(`${API}/estados/changeState`,{
+  const socket = useContext(SocketContext);
+  const [user, setuser] = useState("");
+  const [pass, setpass] = useState("");
+  const [style, setStyle] = useState("sideL");
+  const [showForgot, setForgot] = useState(false);
+  const [showLogin, setShow] = useState(true);
+  const [showChange, setChange] = useState(false);
+  const cambiarestado = async (id, user) => {
+    const res = await fetch(`${API}/estados/changeState`, {
       method: "POST",
       headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify({
-          idestado:id,
-          user:user,
-          responsable:""
-      })
-    })
-    const data =await res.json()
-    console.log(data)
-    socket.emit('Cambio',{'message':data,'room':sessionStorage.getItem('idComp')});
-  }
-        
-    const Navigate = useNavigate();
-    const changePageMenu = useCallback(() => Navigate('/Layout', { replace: true }), [Navigate]);
-    const changePageState = useCallback(() => Navigate('/states', { replace: true }), [Navigate]);
-    const Handlesesion =async (e)=> {
-        e.preventDefault();
-        if(user!="" || pass!=""){
-            const res=await fetch(`${API}/usuario/login`,{
-                method: "POST",
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body:JSON.stringify({
-                    user,
-                    pass
-                })
-            })
-            const r=await res.json();
-            if(r['bool']){
-                if(r['usuario'].perfil==4){
-                    console.log('interprete');
-                    sessionStorage.setItem("user",JSON.stringify(r['usuario']));
-                    sessionStorage.setItem("perfil",r['usuario'].perfil);
-                    sessionStorage.setItem("idComp",r['usuario']['compania']['id']);
-                    cambiarestado(4,JSON.stringify(r['usuario']));
-                    sessionStorage.setItem('diferenciaState',0);
-                    //setStyle("sideLEX"); 
-                    setTimeout(changePageState, 1500);
-                }else{
-                    console.log('admin');
-                    sessionStorage.setItem("user", JSON.stringify(r['usuario']));
-                    sessionStorage.setItem("perfil",r['usuario'].perfil);
-                    sessionStorage.setItem("idComp",r['usuario']['compania']['id']);
-                    sessionStorage.setItem('startDate',new Date(Date.now()))
-                    sessionStorage.setItem('endDate',new Date(Date.now()))
-                    sessionStorage.setItem('reporte',true)
-                    //setStyle("sideLEX"); 
-                    setTimeout(changePageMenu, 1500);
-                }
-            }else{
-                alert(r['response'])
-            }
-            console.log(r);
-        }else{
-            alert("Faltan campos por llenar")
+      body: JSON.stringify({
+        idestado: id,
+        user: user,
+        responsable: "",
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+    socket.emit("Cambio", {
+      message: data,
+      room: sessionStorage.getItem("idComp"),
+    });
+  };
+
+  const Navigate = useNavigate();
+  const changePageMenu = useCallback(
+    () => Navigate("/Layout", { replace: true }),
+    [Navigate]
+  );
+  const changePageState = useCallback(
+    () => Navigate("/states", { replace: true }),
+    [Navigate]
+  );
+  const Handlesesion = async (e) => {
+    e.preventDefault();
+    if (user != "" || pass != "") {
+      const res = await fetch(`${API}/usuario/login`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user,
+          pass,
+        }),
+      });
+      const r = await res.json();
+      if (r["bool"]) {
+        if (r["usuario"].perfil == 4) {
+          console.log("interprete");
+          sessionStorage.setItem("user", JSON.stringify(r["usuario"]));
+          sessionStorage.setItem("perfil", r["usuario"].perfil);
+          sessionStorage.setItem("idComp", r["usuario"]["compania"]["id"]);
+          cambiarestado(4, JSON.stringify(r["usuario"]));
+          sessionStorage.setItem("diferenciaState", 0);
+          //setStyle("sideLEX");
+          setTimeout(changePageState, 1500);
+        } else {
+          console.log("admin");
+          sessionStorage.setItem("user", JSON.stringify(r["usuario"]));
+          sessionStorage.setItem("perfil", r["usuario"].perfil);
+          sessionStorage.setItem("idComp", r["usuario"]["compania"]["id"]);
+          sessionStorage.setItem("startDate", new Date(Date.now()));
+          sessionStorage.setItem("endDate", new Date(Date.now()));
+          sessionStorage.setItem("reporte", true);
+          //setStyle("sideLEX");
+          setTimeout(changePageMenu, 1500);
         }
-
-
+      } else {
+        alert(r["response"]);
+      }
+      console.log(r);
+    } else {
+      alert("Faltan campos por llenar");
     }
+  };
 
   const handleForgot = () => {
-    document.getElementById("contForm").style.display = "none";
-    document.getElementById("formulario2").style.display = "flex";
+    setShow(false);
     setStyle("sideLA");
   };
 
-    const handleLoginS = () => {
-        document.getElementById("contForm").style.display = "flex";
-        document.getElementById("formulario2").style.display = "none";
-        setStyle("sideL");
-    }
+  const handleLoginS = () => {
+    setShow(true);
+    setForgot(false);
+    setChange(false);
+    setStyle("sideL");
+  };
+  const sendEmail = (e) => {
+    setForgot(true);
+    e.preventDefault();
+  };
+  const veriCode = (e) => {
+    setForgot(false);
+    setChange(true);
+    e.preventDefault();
+  };
+  const changePass = () => {
 
-    return (
-        <>
-            <div id="Cont">
-                <div id="Container">
-                    <div className='formulario2' id="formulario2">
-                        <form className='form2'>
-                            <label className='backLogin' onClick={handleLoginS} />
-                            <label className='title'>Recovery Password</label>
-                            <label className='textTitle'>Enter  your Email Adress</label>
-                            <input className='Email' placeholder='Email'></input>
-                            <button className='nextStep' />
-                        </form>
-                    </div>
-                    <div id={style} >
-                        <p></p>
-                    </div>
-                    <div className="contForm" id="contForm">
-                        <form className="Formulario">
-                            <label>Log In</label>
-                            <input placeholder="User" type="email" id="user" onChange={(e) => setuser(e.target.value)}></input>
-                            <input type="password" id="pass" placeholder="Pass" onChange={(e) => setpass(e.target.value)}></input>
-                            <label id="forgePass" onClick={handleForgot}>Forgot your Password?</label>
-                            <input type="submit"onClick={Handlesesion} className="buttonEx" value="Login" />
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  };
+
+  return (
+    <>
+      <div id="Cont">
+        <div id="Container">
+          {showLogin ? (
+            <ShowLogin
+              handleForgot={handleForgot}
+              Handlesesion={Handlesesion}
+            />
+          ) : (
+            ""
+          )}
+          {!showLogin && !showForgot && !showChange ? (
+            <ForgotP
+              title={"Recovery PassWord"}
+              TextCont={"Enter Your Email"}
+              func={sendEmail}
+              back={handleLoginS}
+              placeH={"Email"}
+            />
+          ) : showForgot ? (
+            <ForgotP
+              title={"Verify Code"}
+              TextCont={"enter the code sent to your email"}
+              func={veriCode}
+              back={handleLoginS}
+              placeH={"Code"}
+            />
+          ) : (
+            ""
+          )}
+          {showChange ? (
+            <ForgotP
+              title={"New PassWord"}
+              TextCont={"enter the new Password"}
+              func={changePass}
+              back={handleLoginS}
+              placeH={"New Password"}
+            />
+          ) : (
+            ""
+          )}
+          <div id={style}>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
